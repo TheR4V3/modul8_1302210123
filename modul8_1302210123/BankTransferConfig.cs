1f60ed6f43a628fbad4c6796e46ef8eaab8c07ab
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2023-04-11 17:13:37 +0700

    Aplikasi Bank Transfer dengan runtime configuration

---
+++ modul8_1302210123/BankTransferConfig.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302210123
+{
+    public class AppConfig
+    {
+        public BankTransferConfig config;
+        public transfer config2;
+        public confirmation config3;
+
+        private const string filelocation = "D:\\Code\\TPKPL\\MOD8\\modul8_1302210123\\";
+        private const string filepath = filelocation + "Bank_Transfer_config.json";
+
+        public AppConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                setDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        public void ReadConfigFile()
+        {
+            string result = File.ReadAllText(filepath);
+            config = JsonSerializer.Deserialize<BankTransferConfig>(result);
+        }
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filepath, jsonString);
+        }
+
+        private void setDefault()
+        {
+            config = new BankTransferConfig("en", new List<string> { "RTO (real-time)", "SKN", "RTGS", "BI FAST" }, new transfer(25000000, 6500, 1500), new confirmation("yes", "ya"));
+
+        }
+    }
+    public class transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+        public transfer() { }
+        public transfer(int threshold, int low_fee, int high_fee)
+        {
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+    }
+
+    public class confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+
+        public confirmation() { }
+        public confirmation(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+    }
+    public class BankTransferConfig
+    {
+        public String lang { get; set; }
+        public transfer transfer { get; set; }
+        public List<String> methods { get; set; }
+        public confirmation confirmation { get; set; }
+
+
+
+        public BankTransferConfig()
+        {
+
+        }
+
+        public BankTransferConfig(String lang, List<String> methods, transfer transfer, confirmation confirmation)
+        {
+            this.lang = lang;
+            this.methods = methods;
+            this.transfer = transfer;
+            this.confirmation = confirmation;
+        }
+    }   
+}
--- modul8_1302210123/Program.cs
+++ modul8_1302210123/Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace modul8_1302210123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppConfig Transfer = new AppConfig();
+            if(Transfer.config.lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer");
+            }
+            else
+            {
+                Console.WriteLine("Masukan jumlah uang yang akan ditransfer");
+            }
+
+            int Jumlah;
+            try
+            {
+                Jumlah = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Jumlah = 0;
+            }
+
+            int biayaTransfer;
+            if (Jumlah <= Transfer.config.transfer.threshold)
+            {
+                biayaTransfer = Transfer.config.transfer.threshold;
+            } else
+            {
+                biayaTransfer = Transfer.config.transfer.high_fee;
+            }
+
+            if (Transfer.config.lang == "en") {
+                Console.WriteLine("Transfer fee = " + biayaTransfer);
+                Console.WriteLine("Total amount = " + (biayaTransfer + Jumlah));
+            }
+            else {
+                Console.WriteLine("Biaya transfer = " + biayaTransfer);
+                Console.WriteLine("Total Biaya = " + (biayaTransfer + Jumlah));
+            }
+
+            if (Transfer.config.lang == "en") {
+                Console.WriteLine("Select transfer method:");
+            }
+            else {
+                Console.WriteLine("Pilih metode transfer:");
+            }
+
+            for (int i = 0; i < Transfer.config.methods.Count(); i++)
+            {
+                Console.WriteLine((i + 1)+ Transfer.config.methods[i]);
+            }
+
+            Console.Write("> ");
+            int metodeTransfer;
+            try
+            {
+                metodeTransfer = int.Parse(Console.ReadLine());
+            } catch (FormatException)
+            {
+                metodeTransfer = 1;
+            }
+
+            if (Transfer.config.lang == "en")
+            {
+                Console.Write("Please type " + Transfer.config.confirmation.en + " to confirm the transaction: ");
+            }
+            else
+            {
+                Console.Write("Ketik " + Transfer.config.confirmation.id + " untuk mengkonfirmasi transaksi: ");
+            }
+
+            string inputKonfirmasi = Console.ReadLine();
+
+            if (Transfer.config.lang == "en")
+            {
+                if (inputKonfirmasi.Equals(Transfer.config.confirmation))
+                {
+                    Console.WriteLine("The transfer is completed");
+                } 
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+            else
+            {
+                if (inputKonfirmasi.Equals(Transfer.config.confirmation))
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+        }
+    }
+}
